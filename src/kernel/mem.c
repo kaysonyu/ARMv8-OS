d20994e32dca5a26aefccf4e0b349a9290225407
yuki <2833721527@qq.com>
2022-09-23 11:52:44 +0800

    lab2 improve

--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -21,6 +21,7 @@ define_early_init(alloc_page_cnt)
 
 static QueueNode* pages;
 QueueNode* page_manage [PAGE_NUM][ALLOC_SIZE];
+QueueNode* for_traversal [PAGE_NUM];
 extern char end[];
 define_early_init(pages)
 {   
@@ -90,6 +91,35 @@ void kfree(void* p)
 {   
     _acquire_spinlock(&mem_lock);
     int* int_p = ((int*)p) - 2;
-    add_to_queue(&page_manage[int_p[0]][int_p[1]], (QueueNode*)int_p);
+    int page_order = int_p[0];
+    int size_num = int_p[1];
+    u64 test_p = (u64)int_p;
+    for (int i = size_num; i < ALLOC_SIZE; i++) {
+        int count = 0;
+        int flag_num = -1;
+        for (QueueNode* same_level_p = fetch_all_from_queue(&page_manage[page_order][i]); same_level_p; same_level_p = same_level_p->next) {
+            for_traversal[count] = same_level_p;
+            if (((u64)same_level_p ^ (u64)test_p) == (u64)(1 << (i+3))) {
+                flag_num = count;
+            }
+            count++;  
+        } 
+        for (int j = 0; j < count; j++) {
+            if (j != flag_num) {
+                add_to_queue(&page_manage[page_order][i], for_traversal[j]);
+            }     
+        }   
+        if (flag_num != -1 && i < ALLOC_SIZE-1) {
+            u64 merge_p = (test_p < (u64)for_traversal[flag_num]) ? test_p : (u64)for_traversal[flag_num]; 
+            test_p = merge_p;
+        } 
+        else {
+            add_to_queue(&page_manage[page_order][i], (QueueNode*)test_p);
+            break;
+        }
+    }
+    if (page_manage[page_order][ALLOC_SIZE-1] != NULL) {
+        kfree_page(fetch_from_queue(&page_manage[page_order][ALLOC_SIZE-1]));
+    }
     _release_spinlock(&mem_lock);
 }
